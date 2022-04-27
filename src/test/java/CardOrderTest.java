d2e183d9c6415924e7a28cc1bcc525384d613777
Naan <naanko2021@gmail.com>
2022-04-27 20:03:09 +0300

    Changes CardOrderTest

--- src/test/java/CardOrderTest.java
+++ src/test/java/CardOrderTest.java
@@ -2,9 +2,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardOrderTest {
 
@@ -28,5 +35,21 @@ public class CardOrderTest {
         driver.quit();
         driver = null;
     }
+    @Test
+    void shouldSendForm() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        //driver.findElement().sendKeys("Зинаида Петрова");
+        //driver.findElement().sendKeys( "+79998882233");
+        //List<WebElement> elements =driver.findElements(By.className("input__control"));
+        //elements.get(0).sendKeys("Зинаида Петрова");
+        //elements.get(1).sendKeys("+79998882233");
 
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Зинаида Петрова");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys( "+79998882233");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, text);
+    }
 }
